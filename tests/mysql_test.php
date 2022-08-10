4773104ab7cc6eea86e2b322765c1db4747afafa
evelin <evelinbonfim57@gmail.com>
2022-08-10 12:26:22 -0300

    commit

--- tests/mysql_test.php
+++ tests/mysql_test.php
@@ -5,7 +5,7 @@ require_once '../core/conexao_mysql.php';
 require_once '../core/sql.php';
 require_once '../core/mysql.php';
 
-insert_teste('João', 'joao@ifsp.edu.br', '123456');
+insert_teste('evelin', 'evelinbonfim57@gmail.com', '123456');
 buscar_teste();
 update_teste(38, 'evelin', 'evelinbonfim57@gmail.com');
 buscar_teste();
