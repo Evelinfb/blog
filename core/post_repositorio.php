addb106028415f85d545ddc17371fe7384e241db
evelin <evelinbonfim57@gmail.com>
2022-09-14 12:29:04 -0300

    commit

--- core/post_repositorio.php
+++ core/post_repositorio.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+require_once '../includes/valida_login.php';
 require_once '../includes/funcoes.php';
 require_once 'conexao_mysql.php';
 require_once 'sql.php';
@@ -60,5 +61,5 @@ switch($acao){
 
             break;
 }
-// header('Location: ../index.php');
+header('Location: ../index.php');
 ?>
